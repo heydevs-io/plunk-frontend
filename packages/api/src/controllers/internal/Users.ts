2385795efc4f030297340196e14478a8726b08bd
lau.td <lau.td.developer@gmail.com>
2024-10-15 13:35:26 +0700

    feat: implement create user internal api

--- packages/api/src/controllers/internal/Users.ts
+++ packages/api/src/controllers/internal/Users.ts
@@ -1,8 +1,11 @@
 import { Controller, Middleware, Post } from "@overnightjs/core";
 import type { Request, Response } from "express";
-import { NotFound } from "../../exceptions";
+import { HttpException, NotFound } from "../../exceptions";
 import { isValidInternalSecretKey } from "../../middleware/auth";
 import { UserService } from "../../services/UserService";
+import { UserSchemas } from "@plunk/shared";
+import { prisma } from "../../database/prisma";
+import { createHash } from "../../util/hash";
 
 @Controller("users")
 export class Users {
@@ -20,5 +23,26 @@ export class Users {
 			id: user.id,
 			email: user.email,
 		});
+	}
+
+	@Post("create")
+	@Middleware([isValidInternalSecretKey])
+	public async signup(req: Request, res: Response) {
+		const { email, password } = UserSchemas.credentials.parse(req.body);
+
+		const user = await UserService.email(email);
+
+		if (user) {
+			throw new HttpException(400, "That email is already associated with another user");
+		}
+
+		const created_user = await prisma.user.create({
+			data: {
+				email,
+				password: await createHash(password),
+			},
+		});
+
+		return res.json({ id: created_user.id, email: created_user.email });
 	}
 }
